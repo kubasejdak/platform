715b710117bd7cd2b2f09fa4d40f6113dbf637eb
Kuba Sejdak <kuba.sejdak@gmail.com>
2019-08-11 12:10:05 +0200

    Add tests for platforms.

---
+++ cmake/doxygen.cmake
@@ -0,0 +1,21 @@
+function(doxygen_addTarget INPUT)
+    find_package(Doxygen)
+    if (NOT DOXYGEN_FOUND)
+        message(FATAL_ERROR "Doxygen not found")
+    endif ()
+
+    set(DOXYGEN_OUTPUT_DIRECTORY ${CMAKE_BINARY_DIR}/docs)
+    set(DOXYGEN_GENERATE_LATEX NO)
+    set(DOXYGEN_EXTRACT_PRIVATE YES)
+    set(DOXYGEN_EXTRACT_PACKAGE YES)
+    set(DOXYGEN_EXTRACT_STATIC YES)
+    set(DOXYGEN_WARN_NO_PARAMDOC YES)
+    set(DOXYGEN_HTML_OUTPUT .)
+    set(DOXYGEN_USE_MDFILE_AS_MAINPAGE README.md)
+    set(DOXYGEN_FILE_PATTERNS *.c *.cc *.cxx *.cpp *.c++ *.ii *.ixx *.ipp *.i++ *.inl *.h *.hh *.hxx *.hpp *.h++ *.inc README.md)
+    set(DOXYGEN_EXCLUDE bin/ build/ cmake-build*/ external/ test/ tools/)
+    set(DOXYGEN_WARN_LOGFILE ${DOXYGEN_OUTPUT_DIRECTORY}/doxygen.warn)
+
+    # Add target to generate doxygen docs in the build directory.
+    doxygen_add_docs(doxygen ${INPUT} WORKING_DIRECTORY ${PROJECT_SOURCE_DIR})
+endfunction()
