baf96722fc8e0829d076640218d67af78198b8e2
Kuba Sejdak <kuba.sejdak@gmail.com>
2019-10-13 00:02:33 +0200

    Unify toolchain files across platforms.

--- app/linux-arm/arm-linux-gnueabihf-gcc.cmake
+++ app/linux-arm/arm-linux-gnueabihf-gcc.cmake
@@ -1,23 +1,31 @@
-set(CMAKE_SYSTEM_NAME       Linux)
-set(CMAKE_SYSTEM_PROCESSOR  ARM)
+set(CMAKE_SYSTEM_NAME           Linux)
+set(CMAKE_SYSTEM_PROCESSOR      ARM)
 
 if (WIN32)
-    set(EXE_EXTENSION       ".exe")
+    set(EXE_EXTENSION           ".exe")
 else ()
-    set(EXE_EXTENSION       "")
+    set(EXE_EXTENSION           "")
 endif ()
 
 # Without that flag CMake is not able to pass test compilation check
 set(CMAKE_TRY_COMPILE_TARGET_TYPE STATIC_LIBRARY)
 
-set(CMAKE_AR                arm-linux-gnueabihf-ar${EXE_EXTENSION})
-set(CMAKE_ASM_COMPILER      arm-linux-gnueabihf-as${EXE_EXTENSION})
-set(CMAKE_C_COMPILER        arm-linux-gnueabihf-gcc${EXE_EXTENSION})
-set(CMAKE_CXX_COMPILER      arm-linux-gnueabihf-g++${EXE_EXTENSION})
-set(CMAKE_OBJCOPY           arm-linux-gnueabihf-objcopy${EXE_EXTENSION} CACHE INTERNAL "")
-set(CMAKE_RANLIB            arm-linux-gnueabihf-ranlib${EXE_EXTENSION} CACHE INTERNAL "")
-set(CMAKE_SIZE_UTIL         arm-linux-gnueabihf-size${EXE_EXTENSION} CACHE INTERNAL "")
-set(CMAKE_STRIP             arm-linux-gnueabihf-strip${EXE_EXTENSION} CACHE INTERNAL "")
+set(CMAKE_AR                    ${LINUX_ARM_TOOLCHAIN_PATH}/bin/arm-linux-gnueabihf-ar${EXE_EXTENSION})
+set(CMAKE_ASM_COMPILER          ${LINUX_ARM_TOOLCHAIN_PATH}/bin/arm-linux-gnueabihf-gcc${EXE_EXTENSION})
+set(CMAKE_C_COMPILER            ${LINUX_ARM_TOOLCHAIN_PATH}/bin/arm-linux-gnueabihf-gcc${EXE_EXTENSION})
+set(CMAKE_CXX_COMPILER          ${LINUX_ARM_TOOLCHAIN_PATH}/bin/arm-linux-gnueabihf-g++${EXE_EXTENSION})
+set(CMAKE_OBJCOPY               ${LINUX_ARM_TOOLCHAIN_PATH}/bin/arm-linux-gnueabihf-objcopy${EXE_EXTENSION} CACHE INTERNAL "")
+set(CMAKE_RANLIB                ${LINUX_ARM_TOOLCHAIN_PATH}/bin/arm-linux-gnueabihf-ranlib${EXE_EXTENSION} CACHE INTERNAL "")
+set(CMAKE_SIZE_UTIL             ${LINUX_ARM_TOOLCHAIN_PATH}/bin/arm-linux-gnueabihf-size${EXE_EXTENSION} CACHE INTERNAL "")
+set(CMAKE_STRIP                 ${LINUX_ARM_TOOLCHAIN_PATH}/bin/arm-linux-gnueabihf-strip${EXE_EXTENSION} CACHE INTERNAL "")
+
+set(CMAKE_C_FLAGS               "" CACHE INTERNAL "")
+set(CMAKE_CXX_FLAGS             "${CMAKE_C_FLAGS}" CACHE INTERNAL "")
+
+set(CMAKE_C_FLAGS_DEBUG         "-g -O0" CACHE INTERNAL "")
+set(CMAKE_C_FLAGS_RELEASE       "-O3" CACHE INTERNAL "")
+set(CMAKE_CXX_FLAGS_DEBUG       "${CMAKE_C_FLAGS_DEBUG}" CACHE INTERNAL "")
+set(CMAKE_CXX_FLAGS_RELEASE     "${CMAKE_C_FLAGS_RELEASE}" CACHE INTERNAL "")
 
 set(CMAKE_FIND_ROOT_PATH_MODE_PROGRAM NEVER)
 set(CMAKE_FIND_ROOT_PATH_MODE_LIBRARY ONLY)
