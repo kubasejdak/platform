e528383d1f4f505a6cf0dd3e37b36c3da585fe2b
Kuba Sejdak <kuba.sejdak@gmail.com>
2019-10-20 13:03:43 +0200

    Unify toolchain selection among all platforms.

--- app/linux-arm/toolchain.cmake
+++ app/linux-arm/toolchain.cmake
@@ -1,3 +1,10 @@
 if (NOT DEFINED CMAKE_TOOLCHAIN_FILE)
-    set(CMAKE_TOOLCHAIN_FILE ${CMAKE_CURRENT_LIST_DIR}/arm-linux-gnueabihf-gcc.cmake)
+    set(DEFAULT_TOOLCHAIN       arm-linux-gnueabihf-gcc)
+
+    if (NOT DEFINED TOOLCHAIN)
+        message(STATUS "'TOOLCHAIN' is not defined. Using '${DEFAULT_TOOLCHAIN}'")
+        set(TOOLCHAIN           ${DEFAULT_TOOLCHAIN})
+    endif ()
+
+    set(CMAKE_TOOLCHAIN_FILE    ${CMAKE_CURRENT_LIST_DIR}/${TOOLCHAIN}.cmake)
 endif ()
