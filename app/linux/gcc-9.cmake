a22a67c15789b789ca35a68bca233f165fe65c08
Kuba <kuba.sejdak@gmail.com>
2019-10-21 07:22:57 +0200

    Add basic CI (#1)
    * Create first workflow.
    
    * Add run matrix and print gcc versions.
    
    * Update main.yml
    
    * Check other images.
    
    * Fix CI.
    
    * Fix CI.
    
    * Fix workflow name.
    
    * Use my docker image.
    
    * Checks GCC images.
    
    * Change provider of docker images.
    
    * Add toolchain files for gcc 9, 8 and 7.
    
    * Unify toolchain selection among all platforms.
    
    * Add CI for two baremetal platforms.
    
    * Fix CI.
    
    * Add arm-linux-gnueabihf to CI.

---
+++ app/linux/gcc-9.cmake
@@ -0,0 +1,31 @@
+set(CMAKE_SYSTEM_NAME           Linux)
+
+if (WIN32)
+    set(EXE_EXTENSION           ".exe")
+else ()
+    set(EXE_EXTENSION           "")
+endif ()
+
+# Without that flag CMake is not able to pass test compilation check
+set(CMAKE_TRY_COMPILE_TARGET_TYPE STATIC_LIBRARY)
+
+set(CMAKE_AR                    gcc-ar-9${EXE_EXTENSION})
+set(CMAKE_ASM_COMPILER          gcc-9${EXE_EXTENSION})
+set(CMAKE_C_COMPILER            gcc-9${EXE_EXTENSION})
+set(CMAKE_CXX_COMPILER          g++-9${EXE_EXTENSION})
+set(CMAKE_OBJCOPY               objcopy${EXE_EXTENSION} CACHE INTERNAL "")
+set(CMAKE_RANLIB                ranlib${EXE_EXTENSION} CACHE INTERNAL "")
+set(CMAKE_SIZE_UTIL             size${EXE_EXTENSION} CACHE INTERNAL "")
+set(CMAKE_STRIP                 strip${EXE_EXTENSION} CACHE INTERNAL "")
+
+set(CMAKE_C_FLAGS               "" CACHE INTERNAL "")
+set(CMAKE_CXX_FLAGS             "${CMAKE_C_FLAGS}" CACHE INTERNAL "")
+
+set(CMAKE_C_FLAGS_DEBUG         "-g -O0" CACHE INTERNAL "")
+set(CMAKE_C_FLAGS_RELEASE       "-O3" CACHE INTERNAL "")
+set(CMAKE_CXX_FLAGS_DEBUG       "${CMAKE_C_FLAGS_DEBUG}" CACHE INTERNAL "")
+set(CMAKE_CXX_FLAGS_RELEASE     "${CMAKE_C_FLAGS_RELEASE}" CACHE INTERNAL "")
+
+set(CMAKE_FIND_ROOT_PATH_MODE_PROGRAM NEVER)
+set(CMAKE_FIND_ROOT_PATH_MODE_LIBRARY ONLY)
+set(CMAKE_FIND_ROOT_PATH_MODE_INCLUDE ONLY)
--- app/linux/toolchain.cmake
+++ app/linux/toolchain.cmake
@@ -1,1 +1,10 @@
-# Intentionally left blank.
+if (NOT DEFINED CMAKE_TOOLCHAIN_FILE)
+    set(DEFAULT_TOOLCHAIN       gcc-9)
+
+    if (NOT DEFINED TOOLCHAIN)
+        message(STATUS "'TOOLCHAIN' is not defined. Using '${DEFAULT_TOOLCHAIN}'")
+        set(TOOLCHAIN           ${DEFAULT_TOOLCHAIN})
+    endif ()
+
+    set(CMAKE_TOOLCHAIN_FILE    ${CMAKE_CURRENT_LIST_DIR}/${TOOLCHAIN}.cmake)
+endif ()
