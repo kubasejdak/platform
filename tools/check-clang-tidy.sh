d5da5e928372a480b63dead8354315d70f92e9ac
Kuba <kuba.sejdak@gmail.com>
2019-12-01 15:18:26 +0100

    Add clang tidy (#21)
    * #15 Add clang-tidy checks.
    
    * #15 Fix CI.
    
    *  #15 Fix CI 2.
    
    *  #15 Fix CI 3.
    
    *  #15 Fix CI 4.
    
    *  #15 Fix CI 5.
    
    *  #15 Fix CI 6.
    
    * Disable some clang checks.
    
    * Remove unnecessary step.

---
+++ tools/check-clang-tidy.sh
@@ -0,0 +1,8 @@
+#!/bin/bash
+
+if [ ! -f "compile_commands.json" ]; then
+    echo "No 'compile_commands.json' file found. Aborting."
+    exit 1
+fi
+
+python3 ../tools/run-clang-tidy.py -clang-tidy-binary=clang-tidy-9 -clang-apply-replacements-binary=clang-apply-replacements-9 -quiet -header-filter='platform/app/.*' -export-fixes=errors.yml
