7381f17e80e4ea536530a3063b2b2151f067411b
Kuba Sejdak <kuba.sejdak@gmail.com>
2019-12-27 10:53:48 +0100

    Change compiler names.

--- app/linux-arm/arm-linux-gnueabihf-clang-9.cmake
+++ app/linux-arm/arm-linux-gnueabihf-clang-9.cmake
@@ -13,7 +13,7 @@ set(CMAKE_RANLIB                    llvm-ranlib-9${EXE_EXTENSION} CACHE INTERNAL
 set(CMAKE_SIZE_UTIL                 llvm-size-9${EXE_EXTENSION} CACHE INTERNAL "")
 set(CMAKE_STRIP                     llvm-strip-9${EXE_EXTENSION} CACHE INTERNAL "")
 
-set(CMAKE_C_FLAGS                   "${APP_C_FLAGS} -target arm-linux-gnueabihf --gcc-toolchain=${LINUX_ARM_TOOLCHAIN_PATH} --sysroot=${LINUX_ARM_TOOLCHAIN_PATH}/arm-linux-gnueabihf/libc" CACHE INTERNAL "")
+set(CMAKE_C_FLAGS                   "${APP_C_FLAGS} -target arm-linux-gnueabihf --gcc-toolchain=${LINUX_ARM_TOOLCHAIN_PATH} --sysroot=${LINUX_ARM_TOOLCHAIN_PATH}/arm-none-linux-gnueabihf/libc" CACHE INTERNAL "")
 set(CMAKE_CXX_FLAGS                 "${APP_CXX_FLAGS} ${CMAKE_C_FLAGS}" CACHE INTERNAL "")
 
 set(CMAKE_C_FLAGS_DEBUG             "-g -O0" CACHE INTERNAL "")
@@ -21,7 +21,7 @@ set(CMAKE_C_FLAGS_RELEASE           "-O3" CACHE INTERNAL "")
 set(CMAKE_CXX_FLAGS_DEBUG           "${CMAKE_C_FLAGS_DEBUG}" CACHE INTERNAL "")
 set(CMAKE_CXX_FLAGS_RELEASE         "${CMAKE_C_FLAGS_RELEASE}" CACHE INTERNAL "")
 
-set(CMAKE_EXE_LINKER_FLAGS          "${CMAKE_EXE_LINKER_FLAGS} -fuse-ld=${LINUX_ARM_TOOLCHAIN_PATH}/bin/arm-linux-gnueabihf-ld")
+set(CMAKE_EXE_LINKER_FLAGS          "${CMAKE_EXE_LINKER_FLAGS} -fuse-ld=${LINUX_ARM_TOOLCHAIN_PATH}/bin/arm-none-linux-gnueabihf-ld")
 
 set(CMAKE_FIND_ROOT_PATH_MODE_PROGRAM NEVER)
 set(CMAKE_FIND_ROOT_PATH_MODE_LIBRARY ONLY)
