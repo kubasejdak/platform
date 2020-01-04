5dc1c901c4afeb88e4906df49481c3b36e7e7860
Kuba Sejdak <kuba.sejdak@gmail.com>
2020-01-04 13:45:44 +0100

    Remove conditional compiler paths. Set default APP flags.

--- app/linux/toolchain.cmake
+++ app/linux/toolchain.cmake
@@ -8,3 +8,10 @@ if (NOT DEFINED CMAKE_TOOLCHAIN_FILE)
 
     set(CMAKE_TOOLCHAIN_FILE    ${CMAKE_CURRENT_LIST_DIR}/${TOOLCHAIN}.cmake)
 endif ()
+
+if (NOT DEFINED APP_C_FLAGS)
+    set(APP_C_FLAGS             "")
+endif ()
+if (NOT DEFINED APP_CXX_FLAGS)
+    set(APP_CXX_FLAGS           "")
+endif ()
