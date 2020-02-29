71c8e59e9ae2fc21dc47df9782c5094cbcd01aee
Kuba Sejdak <kuba.sejdak@gmail.com>
2020-02-29 15:59:36 +0100

    Add support for conan profiles.

--- app/freertos-arm/toolchain.cmake
+++ app/freertos-arm/toolchain.cmake
@@ -1,11 +1,12 @@
 if (NOT DEFINED CMAKE_TOOLCHAIN_FILE)
-    set(DEFAULT_TOOLCHAIN       arm-none-eabi-gcc)
+    set(DEFAULT_TOOLCHAIN       arm-none-eabi-gcc-9)
 
     if (NOT DEFINED TOOLCHAIN)
         message(STATUS "'TOOLCHAIN' is not defined. Using '${DEFAULT_TOOLCHAIN}'")
         set(TOOLCHAIN           ${DEFAULT_TOOLCHAIN})
     endif ()
 
+    set(CONAN_PROFILE           ${TOOLCHAIN})
     set(CMAKE_TOOLCHAIN_FILE    ${CMAKE_CURRENT_LIST_DIR}/${TOOLCHAIN}.cmake)
 endif ()
 
