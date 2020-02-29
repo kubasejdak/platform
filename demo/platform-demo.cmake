8898abaccacd2048f411917b489416a38657ddd6
Kuba Sejdak <kuba.sejdak@gmail.com>
2020-02-29 22:27:50 +0100

    Remove unnecessary variable dereferencing.

--- demo/platform-demo.cmake
+++ demo/platform-demo.cmake
@@ -5,7 +5,7 @@ set(BAREMETAL_ARM_TOOLCHAIN_PATH    "/opt/toolchains/gcc-arm-none-eabi-9-2019-q4
 set(FREERTOS_VERSION                freertos-10.2.1)
 set(FREERTOS_PORTABLE               ARM_CM4F)
 
-if (${PLATFORM} STREQUAL baremetal-arm OR ${PLATFORM} STREQUAL freertos-arm)
+if (PLATFORM STREQUAL baremetal-arm OR PLATFORM STREQUAL freertos-arm)
     set(APP_C_FLAGS                 "-mcpu=cortex-m4 -mfloat-abi=hard -mfpu=fpv4-sp-d16 -mthumb" CACHE INTERNAL "")
     set(APP_CXX_FLAGS               "-mcpu=cortex-m4 -mfloat-abi=hard -mfpu=fpv4-sp-d16 -mthumb" CACHE INTERNAL "")
 endif ()
