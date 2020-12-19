c1207952999131da017c7357570181c9a833b825
Kuba Sejdak <kuba.sejdak@gmail.com>
2020-12-19 23:21:48 +0100

    Cleanup.

--- demo/settings.cmake
+++ demo/settings.cmake
@@ -1,4 +1,4 @@
-set(LINUX_ARM_TOOLCHAIN_10_PATH      "/opt/toolchains/gcc-arm-10.2-2020.11-x86_64-arm-none-linux-gnueabihf/bin/")
+set(LINUX_ARM_TOOLCHAIN_10_PATH     "/opt/toolchains/gcc-arm-10.2-2020.11-x86_64-arm-none-linux-gnueabihf/bin/")
 set(BAREMETAL_ARM_TOOLCHAIN_PATH    "/opt/toolchains/gcc-arm-none-eabi-10-2020-q4-major/bin/")
 
 set(FREERTOS_VERSION                freertos-10.2.1)
