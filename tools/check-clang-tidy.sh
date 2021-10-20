eb636364af02574c57eb822f43785e27f574c639
Kuba Sejdak <kuba.sejdak@gmail.com>
2021-10-20 21:51:03 +0000

    Adjust clang configs.

--- tools/check-clang-tidy.sh
+++ tools/check-clang-tidy.sh
@@ -5,4 +5,4 @@ if [[ ! -f "compile_commands.json" ]]; then
     exit 1
 fi
 
-python3 ../tools/run-clang-tidy.py -clang-tidy-binary=clang-tidy-12 -clang-apply-replacements-binary=clang-apply-replacements-12 -quiet -header-filter='platform/app/.*' -export-fixes=errors.yml
+python3 ../tools/run-clang-tidy.py -clang-tidy-binary=clang-tidy-13 -clang-apply-replacements-binary=clang-apply-replacements-13 -quiet -header-filter='platform/app/.*' -export-fixes=errors.yml
