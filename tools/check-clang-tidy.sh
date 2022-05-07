a2b19a0a7f0a8b38da51ea60803d8ada0d2b178f
Kuba Sejdak <kuba.sejdak@gmail.com>
2022-05-07 06:25:02 +0000

    Update toolchains.

--- tools/check-clang-tidy.sh
+++ tools/check-clang-tidy.sh
@@ -1,8 +1,10 @@
 #!/bin/bash
 
+CLANG_VERSION=14
+
 if [[ ! -f "compile_commands.json" ]]; then
     echo "No 'compile_commands.json' file found. Aborting."
     exit 1
 fi
 
-python3 ../tools/run-clang-tidy.py -clang-tidy-binary=clang-tidy-13 -clang-apply-replacements-binary=clang-apply-replacements-13 -quiet -header-filter='platform/app/.*' -export-fixes=errors.yml
+python3 ../tools/run-clang-tidy.py -clang-tidy-binary=clang-tidy-${CLANG_VERSION} -clang-apply-replacements-binary=clang-apply-replacements-${CLANG_VERSION} -quiet -header-filter='platform/app/.*' -export-fixes=errors.yml
