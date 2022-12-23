8b2909310404fb50c8eb4a5dd9a826f6741a4f16
Kuba Sejdak <kuba.sejdak@gmail.com>
2022-12-23 20:10:42 +0000

    Update deps.

--- tools/check-clang-tidy.sh
+++ tools/check-clang-tidy.sh
@@ -7,4 +7,9 @@ if [[ ! -f "compile_commands.json" ]]; then
     exit 1
 fi
 
-python3 ../tools/run-clang-tidy.py -clang-tidy-binary=clang-tidy-${CLANG_VERSION} -clang-apply-replacements-binary=clang-apply-replacements-${CLANG_VERSION} -quiet -header-filter='platform/app/.*' -export-fixes=errors.yml
+python3 ../tools/run-clang-tidy.py -clang-tidy-binary=clang-tidy-${CLANG_VERSION} \
+                                   -clang-apply-replacements-binary=clang-apply-replacements-${CLANG_VERSION} \
+                                   -quiet \
+                                   -use-color \
+                                   -header-filter=$(git rev-parse --show-toplevel)'(/app/|/demo/|/examples/|/lib/|/test/).*' \
+                                   -export-fixes=errors.yml
